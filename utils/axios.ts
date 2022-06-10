4f54ae2be594ef8ca70c086bc5bdcc5ebab408cb
Orlan Quijada <orlantquijada@gmail.com>
2022-06-11 00:47:09 +0800

    authentication

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,22 @@
 import axiosInstance from 'axios'
+import { useAuth } from './auth'
 
 const axios = axiosInstance.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_HOST}`,
+})
+
+axios.interceptors.request.use((config) => {
+  try {
+    // const token = localStorage.getItem('token')
+
+    const token = useAuth.getState().token
+    if (config && config.headers && token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  } catch (error) {
+    return config
+  }
 })
 
 export default axios
